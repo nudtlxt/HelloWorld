ca7bfe55167228a963c76966880f6c986f8bab2f
nudtlxt <nudtlxt@yeah.net>
2017-05-10 16:49:51 -0700

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,39 +1,65 @@
-import numpy as np
-import pygame
-from pygame.locals import *
+#Camera class and container
 
-class bait(object):
+try:
+	import numpy as np
+	import pygame
+	from pygame.locals import *
+except ImportError, err:
+    print "couldn't load module. %s" % (err)
+    sys.exit(2)
+
+def load_png_camera(name):
+    """ Load image and return image object"""
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname)
+        if image.get_alpha is None:
+            image = image.convert()
+        else:
+            image = image.convert_alpha()
+    except pygame.error, message:
+        print 'Cannot load image:', fullname
+        raise SystemExit, message
+    return image, image.get_rect()
+
+class Bait(object):
     #docstring for bait
-    def __init__(self, b_distance):
-        self.b_distance = b_distance
+    def __init__(self, distance):
+        self.distance = distance
+    def bernoulli(self,dis):
+    	#probability decrease as distance increase by exponential distribution
+    	p = np.exp(-lmd*dis)
+    	#draw under bernoulli(p)
+    	return np.random.binomial(1,p,1)
 
+class Camera(pygame.sprite.Sprite):
 
-class camera(pygame.sprite.Sprite):
-
-    #Camera object in the simulation
+    #Camera
     #Returns: Camera object
     #Functions: move, update_pos, update_home, update_sigma
     #Attributes: area, pos, home, sigma
 
-    def __init__(self, pos, c_distance, bait):
+    def __init__(self, pos, distance, bait):
         pygame.sprite.Sprite.__init__(self)
-        self.image, self.rect = load_png('camera.png')
+        self.image, self.rect = load_png_camera('camera.png')
         screen = pygame.display.get_surface()
         self.area = screen.get_rect()
         self.pos = pos
-        self.c_distance = c_distance
+        self.distance = distance
         self.bait = bait
+        self.count = 0
 
-    def move(radius):
-    	pos = hr.binorm_hitrun_circle(self.home,self.sigma,self.pos,self.pos,radius,1)
-    	self.pos = pos
-        return pos
-
-    def update_pos(p):
+    def set_pos(self,p):
     	self.pos = p
+    	self.rect = p
 
-    def update_home(h):
-    	self.home = h
+    def update(self):
+    	#count the number of catching fish
+    	self.count += 1
+    	#change the color of the camera to show catching
 
-    def update_sigma(s):
-    	self.sigma = s
+class Group_Cameras(pygame.sprite.Group):
+	#Container for cameras
+	def __init__(self, arg):
+		pygame.sprite.Group.__init__(self)
+		
