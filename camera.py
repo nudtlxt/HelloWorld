a1ff78bea097c5e4be87e70e254cd88985cee984
nudtlxt <nudtlxt@yeah.net>
2017-05-10 10:57:52 -0700

    Add files via upload

---
+++ camera.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+# [load modules here]
+import pygame
+
+# [resource handling functions here]
+
+class Camera(pygame.sprite.Sprite):
+	"""docstring for Fish"""
+	def __init__(self, arg):
+		pygame.sprite.Sprite.__init__(self)
+		#self.arg = arg
+
+	def update(self):
+		pass
