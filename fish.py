9baa9358610f8f853cbc75f6520fc79b40d533a8
nudtlxt <nudtlxt@yeah.net>
2017-05-10 16:49:20 -0700

    Update fish.py

--- fish.py
+++ fish.py
@@ -1,33 +1,83 @@
-import hit_run as hr
-import numpy as np
-import pygame
-from pygame.locals import *
+#Fish class and container
 
-class fish(pygame.sprite.Sprite):
+try:
+	import os
+	import math
+	import hit_run as hr
+	import numpy as np
+	import pygame
+	from pygame.locals import *
+except ImportError, err:
+    print "couldn't load module. %s" % (err)
+    sys.exit(2)
 
-    #Fish object in the simulation Returns: fish object
+def load_png_fish(name):
+    """ Load image and return image object"""
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname)
+        if image.get_alpha is None:
+            image = image.convert()
+        else:
+            image = image.convert_alpha()
+    except pygame.error, message:
+        print 'Cannot load image:', fullname
+        raise SystemExit, message
+    return image, image.get_rect()
+
+class Fish(pygame.sprite.Sprite):
+
+    #Fish
     #Functions: move, update_pos, update_home, update_sigma
     #Attributes: area, pos, home, sigma
 
-    def __init__(self, pos, home, sigma):
+    def __init__(self, pos, home, sigma,radius, camera):
         pygame.sprite.Sprite.__init__(self)
-        self.image, self.rect = load_png('fish.png')
+        self.image, self.rect = load_png_fish('fish.png')
         screen = pygame.display.get_surface()
         self.area = screen.get_rect()
         self.pos = pos
         self.home = home
         self.sigma = sigma
+        self.radius = radius
+        self.camera = camera
 
-    def move(radius):
-    	pos = hr.binorm_hitrun_circle(self.home,self.sigma,self.pos,self.pos,radius,1)
-    	self.pos = pos
-        return pos
-
-    def update_pos(p):
+    def set_start_pos(self,p):
     	self.pos = p
+    	self.rect = p
 
-    def update_home(h):
+    def set_home(self,h):
     	self.home = h
 
-    def update_sigma(s):
+    def set_sigma(self,s):
     	self.sigma = s
+
+    def set_radius(self,r):
+    	self.radius = r
+
+	def vnorm(self,v):
+		return math.sqrt(sum(v[i]*v[i] for i in range(len(v))))
+
+    def update(self):
+    	pos = hr.binorm_hitrun_circle(self.home,self.sigma,self.pos,self.pos,self.radius,1)
+    	self.pos = pos
+    	self.rect = pos
+
+    	# calculate distance to check whether catched by camera, if catched update camera
+    	d = vnorm(pos - self.camera.pos)
+    	if d<=self.camera.distance:
+    		self.camera.update()
+    	elif d>self.camera.distance and d<=self.camera.bait.distance:
+    		drawbin = self.camera.bait.bernoulli(self.camera.distance-self.camera.bait.distance)
+    		if drawbin:
+    			self.camera.update()
+
+
+
+class Group_fishes(pygame.sprite.Group):
+
+	#Container for fishes
+
+	def __init__(self, arg):
+        pygame.sprite.Group.__init__(self)
+		
