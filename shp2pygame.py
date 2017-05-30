e1337bf7eb54cfe0f8f9f31e0dfbe78ea3aab961
nudtlxt <nudtlxt@yeah.net>
2017-05-29 22:37:15 -0700

    Update shp2pygame.py

--- shp2pygame.py
+++ shp2pygame.py
@@ -1,1 +1,15 @@
 # Connection funcitons between Shapefile and pygame.
+import os
+import geopandas as gp
+from geopy.distance import vincenty
+
+shp = gp.GeoDataFrame.from_file('shapefiles/ctd_data.shp')
+
+corrd1 = (shp.geometry.total_bounds[1],shp.geometry.total_bounds[2])
+corrd2 = (shp.geometry.total_bounds[3],shp.geometry.total_bounds[2])
+corrd3 = (shp.geometry.total_bounds[1],shp.geometry.total_bounds[0])
+corrd4 = (shp.geometry.total_bounds[3],shp.geometry.total_bounds[0])
+
+y_range = vincenty(corrd1,corrd2).m + 100
+x_range = vincenty(corrd1,corrd3).m + 100
+
